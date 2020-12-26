dd5a7603e4c10588c9dee990b71d569a0c250a61
sgi779 <65807226+sgi779@users.noreply.github.com>
2020-12-26 14:41:42 +0530

    script.js

--- script.js
+++ script.js
@@ -7,12 +7,6 @@ const shareData = {
   url: 'https://sgi779.github.io/Capp/',
 }
 
-const btn = document.querySelector('.share');
-
-// Must be triggered some kind of "user activation"
-btn.addEventListener('click', async () => {
-  try {
-    await navigator.share(shareData)
-    resultPara.textContent = 'MDN shared successfully'
-  } 
-});
+function shareMeCapp(){
+navigator.share(shareData)
+}
