307dc9275eebfa709abdf90fa8a2991a7f576a56
sgi779 <65807226+sgi779@users.noreply.github.com>
2020-12-26 14:36:16 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function gopin(lof){
 	location.replace(lof);
 }
+const shareData = {
+  title: 'MDN',
+  text: 'Learn web development on MDN!',
+  url: 'https://developer.mozilla.org',
+}
+
+const btn = document.querySelector('.share');
+
+// Must be triggered some kind of "user activation"
+btn.addEventListener('click', async () => {
+  try {
+    await navigator.share(shareData)
+    resultPara.textContent = 'MDN shared successfully'
+  } 
+});
